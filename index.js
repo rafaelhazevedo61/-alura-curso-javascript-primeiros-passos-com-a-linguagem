d9155111698ae7f377b76c0c249e4614dbcafa1c
rafael <rafaelhazevedo61@gmail.com>
2022-04-24 21:38:24 -0300

    aula 1 - parcial - entendendo o problema do cliente

---
+++ index.js
@@ -0,0 +1,16 @@
+const cliente1Nome = "Ricardo";
+const cliente1CPF = 11122233301;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Alice";
+const cliente2CPF = 11122233302;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+const cliente3Nome = "Alice";
+const cliente3CPF = 11122233302;
+const cliente3Agencia = 1001;
+const cliente3Saldo = 0;
+
+console.log(cliente1Nome);
