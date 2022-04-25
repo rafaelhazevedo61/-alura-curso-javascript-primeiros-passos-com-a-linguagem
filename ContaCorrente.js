64b8f25cd324c8951c0821707c4895c308729865
rafael <rafaelhazevedo61@gmail.com>
2022-04-25 00:49:44 -0300

    aula 5 - parcial - construtores

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -4,20 +4,25 @@ export class ContaCorrente {
     agencia;
     _cliente;
 
-    set cliente(novoValor){
-        if(novoValor instanceof Cliente){
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
             this._cliente = novoValor;
         }
     }
 
-    get cliente(){
+    get cliente() {
         return this._cliente;
     }
 
     _saldo = 0;
 
-    get saldo(){
+    get saldo() {
         return this._saldo;
+    }
+
+    constructor(cliente, agencia){
+        this.agencia = agencia;
+        this.cliente = cliente;
     }
 
     sacar(valor) {
@@ -28,15 +33,15 @@ export class ContaCorrente {
     }
 
     depositar(valor) {
-        if (valor <= 0)  return;
-/*         if (valor <= 0) {
-            return;
-        } */
+        if (valor <= 0) return;
+        /*         if (valor <= 0) {
+                    return;
+                } */
         this._saldo += valor;
 
     }
 
-    transferir(valor,conta){
+    transferir(valor, conta) {
 
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
