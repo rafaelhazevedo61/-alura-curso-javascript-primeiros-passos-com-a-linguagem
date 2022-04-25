1a0ddf4d2cab531e0c16ae78dca353e86fa77d7f
rafael <rafaelhazevedo61@gmail.com>
2022-04-24 22:22:45 -0300

    aula 2 - parcial - comportamento de classes

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-class cliente {
+class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
-    rg;
 }
 
-const cliente1 = new cliente();
-const cliente2 = new cliente();
+class ContaCorrente {
+    agencia;
+    saldo;
+
+    sacar(valor){
+        if (this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233301;
@@ -19,9 +27,20 @@ cliente2.cpf = 11122233302;
 cliente2.agencia = 1001;
 cliente2.saldo = 0;
 
-const cliente3Nome = "Alice";
-const cliente3CPF = 11122233302;
-const cliente3Agencia = 1001;
-const cliente3Saldo = 0;
+const contaCorrenteRicardo = new ContaCorrente();
 
-console.log(cliente1, cliente2);
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+console.log(contaCorrenteRicardo.saldo);
+
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+
+let valorSacado = 200;
+
+contaCorrenteRicardo.sacar(valorSacado);
+console.log(contaCorrenteRicardo.saldo);
+
+
+console.log(cliente1);
+console.log(cliente2);
