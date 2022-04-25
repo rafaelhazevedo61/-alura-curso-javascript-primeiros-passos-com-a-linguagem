87d7737a6d13c4c455d6771e74280f11c1685dab
rafael <rafaelhazevedo61@gmail.com>
2022-04-24 22:35:13 -0300

    aula 2 - parcial - atributos privados

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ class Cliente {
 
 class ContaCorrente {
     agencia;
-    saldo;
+    _saldo = 0;
 
     sacar(valor){
-        if (this.saldo >= valor){
-            this.saldo -= valor;
+        if (this._saldo >= valor){
+            this._saldo -= valor;
+            console.log("valor total apos saque: "+this._saldo);
+        }
+    }
+
+    depositar(valor){
+        if(valor > 0){
+            this._saldo += valor;
+            console.log("valor valor total apos deposito: "+this._saldo);
         }
     }
 }
@@ -28,19 +36,11 @@ cliente2.agencia = 1001;
 cliente2.saldo = 0;
 
 const contaCorrenteRicardo = new ContaCorrente();
-
-contaCorrenteRicardo.saldo = 0;
 contaCorrenteRicardo.agencia = 1001;
-console.log(contaCorrenteRicardo.saldo);
 
-contaCorrenteRicardo.saldo = 100;
-console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.sacar(50);
 
-let valorSacado = 200;
-
-contaCorrenteRicardo.sacar(valorSacado);
-console.log(contaCorrenteRicardo.saldo);
-
-
-console.log(cliente1);
-console.log(cliente2);
+console.log(contaCorrenteRicardo);
